6a8638672f76cc0db574c7547ea618d4629c912c
joannaberlin <joanna.kot.ok@gmail.com>
2023-03-27 18:44:01 +0200

    Add logout button

--- frontend/src/components/UI/Hero/Hero.js
+++ frontend/src/components/UI/Hero/Hero.js
@@ -1,11 +1,21 @@
+import { Form, useRouteLoaderData } from 'react-router-dom';
 import styles from './Hero.module.scss';
 import logo from '../../../assets/LiftUpLogo.png';
 
 const Hero = () => {
+	const token = useRouteLoaderData('root');
+
 	return (
-		<div className={styles.image_wrapper}>
-			<img src={logo} alt='Dumbbell with wings'></img>
-		</div>
+		<>
+			<div className={styles.image_wrapper}>
+				<img src={logo} alt='Dumbbell with wings'></img>
+			</div>
+			{token && (
+				<Form action='/logout' method='post'>
+					<button>Log out</button>
+				</Form>
+			)}
+		</>
 	);
 };
 
